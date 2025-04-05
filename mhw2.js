3a055cea1a7fe0ccbdee60db42bd11043dd199a0
tiziano-viglianisi <vgltzn03p13c351a@studium.unict.it>
2025-04-05 11:53:05 +0200

    commit2

--- mhw2.js
+++ mhw2.js
@@ -2,6 +2,7 @@ function aprimenu(event) {
     const burger = document.querySelector("#burger");
     const menutendina = document.querySelector("#menutendina");
     const studiumimage=document.createElement("img");
+    const bigbox=document.querySelector("#home-bigbox")
     studiumimage.src="studium.png";
     studiumimage.style.width="50px";
     studiumimage.style.height="50px";
@@ -9,18 +10,21 @@ function aprimenu(event) {
     menutendina.prepend(studiumimage);
     menutendina.classList.remove("hidden");
     burger.classList.add("hidden");
+    bigbox.style.justifyContent="right";
     event.stopPropagation();
-
     document.addEventListener("click", chiudiMenu);
 }
+
 function chiudiMenu(event){
     const menu = document.querySelector("#menutendina");
         if (!menu.contains(event.target)) {
             const burger = document.querySelector("#burger");
             const studiumimage=document.querySelector("img[src='studium.png']");
+            const bigbox=document.querySelector("#home-bigbox")
             studiumimage.remove();
             menutendina.classList.add("hidden");
             burger.classList.remove("hidden");
+            bigbox.style.justifyContent="space-between";
             event.stopPropagation();
         }
 }
